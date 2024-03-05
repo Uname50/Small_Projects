9b6d009ffac179e94afd791d4731561bfd5a860c
Uname50 <158527626+Uname50@users.noreply.github.com>
2024-03-04 23:27:34 -0500

    initial commit

---
+++ Coffee_Machine.py
@@ -0,0 +1,37 @@
+"""
+Coffee machine sells coffee! 
+Before making a sale, it checks if there are enough resources to make a cup of required coffee, 
+Then takes money, gives back change, if any, and serves coffee
+"""
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
