b716669d115a7788a6ff8147b56a365fe9c271bf
Uname50 <158527626+Uname50@users.noreply.github.com>
2024-04-01 15:59:33 -0400

    pandas practice

--- Pandas_Test.py
+++ Pandas_Test.py
@@ -23,4 +23,50 @@ selected_column = test_dataframe[['Name']]
 
 # access multiple columns of a dataframe 
 multiple_columns = test_dataframe[['Name', 'ID', 'Department']]
-print(multiple_columns)
+# print(multiple_columns)
+
+
+# Problem 1: Create a dataframe to display the result 
+practice_dictionary = {
+    'Student': ['David', 'Samuel', 'Terry', 'Evan'],
+    'Age': [27, 24, 22, 32],
+    'Country': ['UK', 'Canada', 'Spain', 'USA'],
+    'Course': ['Python', 'Data Structures', 'Machine Learning', 'Web Development'],
+    'Marks': [85, 72, 89, 76]
+} 
+
+practice_dataframe = pd.DataFrame(practice_dictionary)
+
+# Problem 2: Retrieve the Marks column and assign it to a variable b
+b = practice_dataframe[['Marks']]
+
+# Problem 3: Retrieve the Country and Course columns and assign it to a variable c
+c = practice_dataframe[['Country', 'Course']]
+
+
+# To view the column as a series, just use one bracket
+x = practice_dataframe['Student']
+# print(x, type(x))
+
+"""
+loc() is a label-based data selecting method. Syntax: loc[row_label, column_label]
+iloc() is an index-based selecting method. Syntax: iloc[row_index, column_index]
+"""
+
+# access the value on the first row and the first column
+practice_dataframe.iloc[0, 0]
+
+# access the value on the first row and the third column
+practice_dataframe.iloc[0, 2]
+
+# access the column using name
+practice_dataframe.loc[0, 'Marks']
+
+df2 = practice_dataframe
+df2 = df2.set_index("Student")
+
+# access first 5 rows of a dataframe
+df2.head()
+
+# access the column using name
+print(df2.loc['David', 'Marks'])
